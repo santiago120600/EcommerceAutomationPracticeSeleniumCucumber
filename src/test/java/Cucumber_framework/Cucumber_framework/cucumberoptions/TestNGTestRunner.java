fe197faef0edf1323ab3f12b6ee35087b332ccf7
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2024-01-30 17:52:02 -0600

    added parallel execution

--- src/test/java/Cucumber_framework/Cucumber_framework/cucumberoptions/TestNGTestRunner.java
+++ src/test/java/Cucumber_framework/Cucumber_framework/cucumberoptions/TestNGTestRunner.java
@@ -1,5 +1,7 @@
 package Cucumber_framework.Cucumber_framework.cucumberoptions;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -8,5 +10,10 @@ glue = "Cucumber_framework/Cucumber_framework/stepdefinitions",
 plugin= {"pretty","html:target/cucumber.html","json:target/cucumber.json"},
 monochrome = true)
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
+	
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
 }

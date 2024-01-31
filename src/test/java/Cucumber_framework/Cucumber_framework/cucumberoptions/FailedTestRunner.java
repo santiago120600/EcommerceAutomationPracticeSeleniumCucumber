f1be0257146f1e84f59ce83a1d3055dc5597066c
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2024-01-31 14:24:08 -0600

    added re run configuration

--- src/test/java/Cucumber_framework/Cucumber_framework/cucumberoptions/FailedTestRunner.java
+++ src/test/java/Cucumber_framework/Cucumber_framework/cucumberoptions/FailedTestRunner.java
@@ -5,16 +5,13 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/Cucumber_framework/Cucumber_framework/features",
+@CucumberOptions(features = "@target/failed_scenarios.txt",
 glue = "Cucumber_framework/Cucumber_framework/stepdefinitions",
 plugin= {"pretty","html:target/cucumber.html","json:target/cucumber.json",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"rerun:target/failed_scenarios.txt"
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 		},
-tags="@Checkout or @SearchProducts",
-monochrome = false,
-dryRun = false)
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+monochrome = false)
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
 	
 	@Override
 	@DataProvider(parallel = false)

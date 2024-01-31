4d39367e83d7887c405de8d71a80caa68bc7d9b9
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2024-01-31 13:16:03 -0600

    added extent reports

--- src/test/java/Cucumber_framework/Cucumber_framework/cucumberoptions/TestNGTestRunner.java
+++ src/test/java/Cucumber_framework/Cucumber_framework/cucumberoptions/TestNGTestRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/Cucumber_framework/Cucumber_framework/features",
 glue = "Cucumber_framework/Cucumber_framework/stepdefinitions",
-plugin= {"pretty","html:target/cucumber.html","json:target/cucumber.json"},
+plugin= {"pretty","html:target/cucumber.html","json:target/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+		},
 tags="@Checkout or @SearchProducts",
 monochrome = false,
 dryRun = false)
--- src/test/java/Cucumber_framework/Cucumber_framework/stepdefinitions/Hooks.java
+++ src/test/java/Cucumber_framework/Cucumber_framework/stepdefinitions/Hooks.java
@@ -1,18 +1,32 @@
 package Cucumber_framework.Cucumber_framework.stepdefinitions;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import Cucumber_framework.Cucumber_framework.utils.TestContextSetup;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
-	
+
 	TestContextSetup testContextSetup;
-	 
-	
+
 	public Hooks(TestContextSetup testContextSetup) {
 		super();
 		this.testContextSetup = testContextSetup;
 	}
 
+	@AfterStep
+	public void takeScraenshotOnFailure(Scenario scenario) {
+		WebDriver driver = testContextSetup.base.webDriverManager();
+		if (scenario.isFailed()) {
+			TakesScreenshot ts = (TakesScreenshot) driver;
+			byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+			scenario.attach(src, "image/png", "screenshot");
+		}
+	}
 
 	@After
 	public void tearDown() {
